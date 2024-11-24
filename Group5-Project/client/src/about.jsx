c05335f9de2be8d6e6ecc04d34381a605ec9b1f9
KopilaKR <sung192555@gmail.com>
2024-11-24 17:33:30 -0500

    Delete Group5-Project directory

--- Group5-Project/client/src/about.jsx
+++
@@ -1,6 +0,0 @@
-export default function About() {
-     return <>
-          <h1>Website information will be located here.</h1>
-     </>
-    }
-    
--- Group5-Project/client/src/services.jsx
+++
@@ -1,7 +0,0 @@
-export default function Services() {
-  return (
-   <>
-    <h1>Services that we are going to offer will be located here</h1>
-   </>
-  );
- }

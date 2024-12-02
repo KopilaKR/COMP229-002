fb40b23498b9acd0d895148c4c0e781cc9cd7aa2
Jongho Baek <sung192555@gmail.com>
2024-12-02 18:52:15 -0500

    dev

--- Group5-Project/server/express.js
+++ Group5-Project/server/express.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
@@ -10,6 +11,7 @@ import contactRoutes from './routes/contact.routes.js';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
+const CURRENT_WORKING_DIR = process.cwd();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,5 +24,12 @@ app.use(cors());
 app.use('/', userRoutes);
 app.use('/', contactRoutes);
 app.use('/auth', authRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(CURRENT_WORKING_DIR, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(CURRENT_WORKING_DIR, 'client', 'dist', 'index.html'));
+});
 
 export default app;
